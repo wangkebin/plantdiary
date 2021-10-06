40228f88bcd76d0bdec9cc28bd45c5f88187838f
wangkebin <kebinwang@gmail.com>
2021-10-05 22:30:39 -0400

    Initial commit

---
+++ src/test/java/com/kebin/plantdiary/PlantdiaryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kebin.plantdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantdiaryApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
